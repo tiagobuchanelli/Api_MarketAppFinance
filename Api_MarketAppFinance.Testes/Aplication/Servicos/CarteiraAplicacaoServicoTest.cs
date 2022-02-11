137bbb963ed6b2e06c5e452fa5a8c846ffa98d12
tiago <tiagobuchanelli@gmail.com>
2022-02-11 00:01:09 -0300

    Refatorado Teste - CarteiraAplicacaoServico

--- Api_MarketAppFinance.Testes/Aplication/Servicos/CarteiraAplicacaoServicoTest.cs
+++ Api_MarketAppFinance.Testes/Aplication/Servicos/CarteiraAplicacaoServicoTest.cs
@@ -1,23 +1,49 @@
+using Api_MarketAppFinance.Application;
+using Api_MarketAppFinance.Application.Dtos;
+using Api_MarketAppFinance.Application.Mappers;
 using Api_MarketAppFinance.Domain.Entities;
 using Api_MarketAppFinance.Domain.Interrfaces.Repositories;
 using Api_MarketAppFinance.Domain.Services;
+using AutoMapper;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Api_MarketAppFinance.Testes
 {
-    public class CarteiraServicoTest
+    public class CarteiraAplicacaoServicoTest
     {
-        public CarteiraServicoTest() {}
+        private IMapper _mapper;
+
+        public CarteiraAplicacaoServicoTest() 
+        {
+            SetMapper();
+        }
+
+        private void SetMapper()
+        {
+            var _autoMapperProfile = new DtoParaEntidadeCarteira();
+            var _autoMapperProfile2 = new EntidadeCarteiraParaDto();
+            var _configuration = new MapperConfiguration(x =>
+            {
+                x.AddProfile(_autoMapperProfile);
+                x.AddProfile(_autoMapperProfile2);
+            }
+
+            );
+            _mapper = new Mapper(_configuration);
+        }
 
         [Fact]
         public void Adicionar_Carteira_RetornarExcecaoNome()
         {
-            var mockServico = new CarteiraServico(new Mock<ICarteiraRepositorio<Carteira>>().Object);
+            var mockRepositorio = new Mock<ICarteiraRepositorio<Carteira>>();
+            var mockServico = new CarteiraServico(mockRepositorio.Object);
+            var mockAplicacao = new CarteiraAplicacaoServico(mockServico, _mapper);
 
-            var excecao = Assert.Throws<Exception>(() => mockServico.AdicionarCarteira(new Carteira("", 1)));
+            var excecao = Assert.Throws<Exception>(() => mockAplicacao.Adicionar(_mapper.Map<CarteiraDto>(new Carteira("", 0))));
 
             Assert.Equal("Obrigatório informar o nome.", excecao.Message);  
         }
@@ -25,9 +51,11 @@ namespace Api_MarketAppFinance.Testes
         [Fact]
         public void Adicionar_Carteira_RetornarExcecaoEmpresa()
         {
-            var mockServico = new CarteiraServico(new Mock<ICarteiraRepositorio<Carteira>>().Object);
+            var mockRepositorio = new Mock<ICarteiraRepositorio<Carteira>>();
+            var mockServico = new CarteiraServico(mockRepositorio.Object);
+            var mockAplicacao = new CarteiraAplicacaoServico(mockServico, _mapper);
 
-            var excecao = Assert.Throws<Exception>(() => mockServico.AdicionarCarteira(new Carteira("Dinheiro", 0)));
+            var excecao = Assert.Throws<Exception>(() => mockAplicacao.Adicionar(_mapper.Map<CarteiraDto>(new Carteira("Dinheiro", 0))));
 
             Assert.Equal("Obrigatório informar uma empresa válida.", excecao.Message);
         }
@@ -36,13 +64,16 @@ namespace Api_MarketAppFinance.Testes
         [Fact]
         public void Atualizar_Carteira_RetornarExcecao()
         {
-            var mockServico = new CarteiraServico(new Mock<ICarteiraRepositorio<Carteira>>().Object);
-
             var carteira = new Carteira("Dinheiro", 1);
             var prop = typeof(Carteira).GetProperty(nameof(Carteira.Id));
             if (prop is not null) prop.SetValue(carteira, 1, null);
 
-            var excecao = Assert.Throws<Exception>(() => mockServico.AtualizarCarteira(carteira));
+            var mockRepositorio = new Mock<ICarteiraRepositorio<Carteira>>();            
+            var mockServico = new CarteiraServico(mockRepositorio.Object);
+            var mockAplicacao = new CarteiraAplicacaoServico(mockServico, _mapper);          
+           
+
+            var excecao = Assert.Throws<Exception>(() => mockAplicacao.Atualizar(_mapper.Map<CarteiraDto>(carteira)));
 
             Assert.Equal("Nenhuma carteira encontrada", excecao.Message);
         }
@@ -55,10 +86,11 @@ namespace Api_MarketAppFinance.Testes
             if (prop is not null) prop.SetValue(carteira, 1, null);
             
             var mockRepositorio = new Mock<ICarteiraRepositorio<Carteira>>();
-            mockRepositorio.Setup(x => x.BuscarPorCodigo(1,1)).Returns(carteira);            
+            mockRepositorio.Setup(x => x.BuscarPorCodigo(1,1)).Returns(carteira);
+            var mockServico = new CarteiraServico(mockRepositorio.Object);
+            var mockAplicacao = new CarteiraAplicacaoServico(mockServico, _mapper);
 
-            var mockServico = new CarteiraServico(mockRepositorio.Object);             
-            var retorno = mockServico.AtualizarCarteira(carteira);
+            var retorno = mockAplicacao.Atualizar(_mapper.Map<CarteiraDto>(carteira));
 
             Assert.True(retorno is not null);
         }
@@ -72,10 +104,11 @@ namespace Api_MarketAppFinance.Testes
 
             var mockRepositorio = new Mock<ICarteiraRepositorio<Carteira>>();
             mockRepositorio.Setup(x => x.BuscarPorCodigo(1, 1)).Returns(carteira);
+            var mockServico = new CarteiraServico(mockRepositorio.Object);
+            var mockAplicacao = new CarteiraAplicacaoServico(mockServico, _mapper);
 
-            var mockServico = new CarteiraServico(mockRepositorio.Object);            
             carteira.AlterarDescricao("Cheque");
-            var retorno = mockServico.AtualizarCarteira(carteira);
+            var retorno = mockAplicacao.Atualizar(_mapper.Map<CarteiraDto>(carteira));
 
             Assert.True(retorno.Descricao == "Cheque");
         }        
@@ -89,11 +122,11 @@ namespace Api_MarketAppFinance.Testes
 
             var mockRepositorio = new Mock<ICarteiraRepositorio<Carteira>>();
             mockRepositorio.Setup(x => x.BuscarCarteiras(1)).Returns(lista);
-
             var mockServico = new CarteiraServico(mockRepositorio.Object);
-            var retorno = mockServico.BuscarCarteiras(1);
+            var mockAplicacao = new CarteiraAplicacaoServico(mockServico, _mapper);
 
-            Assert.True(retorno.Count > 0);
+            var retorno = mockAplicacao.BuscarCarteiras(1);
+            Assert.True(retorno.ToList().Count > 0);
         }
 
         [Fact]
@@ -104,10 +137,11 @@ namespace Api_MarketAppFinance.Testes
             lista.Add(new Carteira("Dinheiro2", 1));
 
             var mockRepositorio = new Mock<ICarteiraRepositorio<Carteira>>();
-            mockRepositorio.Setup(x => x.BuscarCarteiras(1)).Returns(lista);
-
+            mockRepositorio.Setup(x => x.BuscarCarteiras(1)).Returns(lista); 
             var mockServico = new CarteiraServico(mockRepositorio.Object);
-            var excecao = Assert.Throws<Exception>(() => mockServico.BuscarCarteiras(2));
+            var mockAplicacao = new CarteiraAplicacaoServico(mockServico, _mapper);
+
+            var excecao = Assert.Throws<Exception>(() => mockAplicacao.BuscarCarteiras(2));
 
             Assert.Equal("Nenhuma carteira encontrada", excecao.Message);
         }
@@ -121,9 +155,10 @@ namespace Api_MarketAppFinance.Testes
 
             var mockRepositorio = new Mock<ICarteiraRepositorio<Carteira>>();
             mockRepositorio.Setup(x => x.BuscarPorCodigo(1, 1)).Returns(carteira);
+            var mockServico = new CarteiraServico(mockRepositorio.Object);
+            var mockAplicacao = new CarteiraAplicacaoServico(mockServico, _mapper);
 
-            var mockServico = new CarteiraServico(mockRepositorio.Object); 
-            var excecao = Assert.Throws<Exception>(() => mockServico.BuscarPorCodigo(1, 2));
+            var excecao = Assert.Throws<Exception>(() => mockAplicacao.BuscarPorCodigo(1, 2));
 
             Assert.Equal("Nenhuma carteira encontrada", excecao.Message);
         }
@@ -137,11 +172,13 @@ namespace Api_MarketAppFinance.Testes
 
             var mockRepositorio = new Mock<ICarteiraRepositorio<Carteira>>();
             mockRepositorio.Setup(x => x.BuscarPorCodigo(1, 1)).Returns(carteira);
-
             var mockServico = new CarteiraServico(mockRepositorio.Object);
-            var retorno = mockServico.BuscarPorCodigo(1, 1);
+            var mockAplicacao = new CarteiraAplicacaoServico(mockServico, _mapper);
+
+            var retorno = mockAplicacao.BuscarPorCodigo(1, 1);
 
             Assert.True(retorno is not null);
         }
+       
     }
 }
