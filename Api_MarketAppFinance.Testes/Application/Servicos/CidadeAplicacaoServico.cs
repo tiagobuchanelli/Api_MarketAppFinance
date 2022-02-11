f8249c9ac48ed073832d848f682df3898965de10
tiago <tiagobuchanelli@gmail.com>
2022-02-11 15:31:34 -0300

    Refactor Ctor

---
+++ Api_MarketAppFinance.Testes/Application/Servicos/CidadeAplicacaoServico.cs
@@ -0,0 +1,17 @@
+﻿using Api_MarketAppFinance.Testes.MapperMock;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api_MarketAppFinance.Testes.Application.Servicos
+{
+    public class CidadeAplicacaoServico
+    {
+        private IMapper _mapper;
+
+        public CidadeAplicacaoServico() => _mapper = new MockMapper().ObterMappers();
+    }
+}
